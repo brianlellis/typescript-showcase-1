8c01229726ea9e1b6fb4c8187b7711dd253a7f94
Brian Ellis <brian@briansresume.com>
2021-09-12 16:58:43 -0400

    npm-watch script config

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 "use strict";
 function hello() {
-    let return_let      = 'hello';
-    const RETURN_CONST  = ' world!';
-    console.log(return_let+return_const);
+    var return_let = 'hello';
+    var RETURN_CONST = ' world!';
+    console.log(return_let + RETURN_CONST);
 }
 hello();
 //# sourceMappingURL=index.js.map
