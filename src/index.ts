9bd98af80dc9dd8f5aa3bd2b70aa4db98c0c1a26
Brian Ellis <brian@briansresume.com>
2021-09-12 16:45:57 -0400

    eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello() {
-    let return_let:     string  = 'hello';
-    const RETURN_CONST: string  = ' world!';
+    let return_let:     string = 'hello';
+    const RETURN_CONST: string = ' world!';
     console.log(return_let+RETURN_CONST);
 }
 hello();
